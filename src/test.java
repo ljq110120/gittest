d9b224455bd83536c91b91463a44c60e0da559be
leijiaqi <leijiaqi@111.com.cn>
2021-05-03 15:39:42 +0800

    add test file

---
+++ src/test.java
@@ -0,0 +1,10 @@
+/**
+ * @Auther: leijiaqi
+ * @Date: 2021/05/03/15:38
+ * @Description:
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
